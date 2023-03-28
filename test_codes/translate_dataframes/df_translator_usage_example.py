593c436bc868e05f1d20754901352cd04df6294a
AxelV369 <axel.vargas92@live.com>
2023-03-27 22:05:38 -0600

    example folders

---
+++ test_codes/translate_dataframes/df_translator_usage_example.py
@@ -0,0 +1,32 @@
+# This script is designed to translate the contents of a specific column in a given CSV file into a specified target language. 
+# The input CSV file should contain a column with the project names to be translated.
+
+import os
+import pandas as pd
+from unlimited_machine_translator.translator import machine_translator_df, merge_csvs, store_translation
+from deep_translator import GoogleTranslator
+
+# Input information
+root = os.getcwd()                               # working directory
+data_set_name = 'sample_data_project_names.csv'  # data with the column to be translated (must be in a csv file)
+# Translate specs
+target_language = 'en'                           # target language
+column_name = 'Project_Name'                     # column to be translated
+Translator = GoogleTranslator                    # Available translators: GoogleTranslator, MicrosoftTranslator, PonsTranslator, LingueeTranslator, MyMemoryTranslator, 
+                                                 #                        YandexTranslator, PapagoTranslator, DeeplTranslator, QcriTranslator,
+# Output information
+output_name = target_language.upper() + '_' + data_set_name
+
+
+# ---------------------------------- Dataset read -----------------------------------------------
+data_set = pd.read_csv(os.path.join(root, data_set_name))
+
+# -------------------- Translate datset by batches (stored in multiple .csv) ---------------------
+stored_location = machine_translator_df(target_language, root, data_set, column_name, output_name, Translator)
+
+# ----------------------------- Merge the translated .csv files -----------------------------------
+df_merged_translation = merge_csvs(target_language, stored_location, data_set, column_name)
+print(df_merged_translation.head())
+
+# --------------------------- Store everything in one output file ---------------------------------
+store_translation(df_merged_translation, root, output_name)
---
+++ test_codes/translate_docs/doc_translator_usage_example.py
@@ -0,0 +1,24 @@
+import os
+from unlimited_machine_translator.translator import read_word_document, save_text_to_docx, machine_translator_doc
+from deep_translator import GoogleTranslator
+                             
+# Input information
+root = os.getcwd()                               # working directory
+data_name = 'Importancia_pipelines.docx'      # data with the column to be translated (must be in a csv file)
+# Translate specs
+target_language = 'en'                           # target language
+Translator = GoogleTranslator                    # Available translators: GoogleTranslator, MicrosoftTranslator, PonsTranslator, LingueeTranslator, MyMemoryTranslator, 
+                                                 #                        YandexTranslator, PapagoTranslator, DeeplTranslator, QcriTranslator,
+# Output information
+output_name = target_language.upper() + '_' + data_name
+
+
+# ---------------------------------- Dataset read -----------------------------------------------
+text = read_word_document(root, data_name)
+
+# ----------------------------- Translate the text ----------------------------------------------  
+translated_text = machine_translator_doc(target_language, root, text, data_name, Translator)
+print(translated_text[:100], '...\n')
+
+# --------------------------- Store everything in one output file -------------------------------
+save_text_to_docx(root, translated_text, output_name)
