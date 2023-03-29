d166c75830698c4b7740c231dd082a424961933a
AxelV369 <axel.vargas92@live.com>
2023-03-28 19:10:03 -0600

    version setup changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="unlimited_machine_translator",
-    version="1.1.0",
+    version="1.1.1",
     description="A package for unlimited machine translation",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
